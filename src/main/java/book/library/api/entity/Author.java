48074367e4f7db01890ca66845fd95c578a172dd
SergeiGorvat <gorvat2012@yandex.ru>
2026-02-27 13:56:08 +0300

    [java-spring-mvc-task2] Добавил модельки Author и Book. Дополнил настройки с бд.

--- src/main/java/book/library/api/entity/Author.java
+++ src/main/java/book/library/api/entity/Author.java
@@ -1,4 +1,38 @@
 package book.library.api.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "authors")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    @Builder.Default
+    private List<Book> books = new ArrayList<>();
 }
--- src/main/java/book/library/api/entity/Book.java
+++ src/main/java/book/library/api/entity/Book.java
@@ -1,4 +1,35 @@
 package book.library.api.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "books")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false)
+    private String genre;
+
+    @Column(nullable = false)
+    private Integer year;
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author author;
 }
